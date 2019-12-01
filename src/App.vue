9cf6bacaa0086fb183eff3d9a1346efee88dafa2
nlebedevinc <mikolka96parovoz@gmail.com>
2019-12-01 22:48:54 +0300

    Include normalize css and layout

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,22 @@
       :clickEffect="false"
       color="#dedede"
     />
+
+    <div class="card">
+      <h1>Nikolay Lebedev</h1>
+      <h2>nlebedevinc</h2>
+
+      <div class="card-icons">
+        <a href="https://github.com/nlebedevinc">
+            <icon name="brands/github" scale="2" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import 'normalize.css/normalize.css'
 // import HelloWorld from './components/HelloWorld.vue'
 import Particles from './components/Particles.vue'
 
@@ -22,13 +34,50 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+html {
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
 }
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+#app {
+  background-color: #222;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  #particles-js {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    canvas {
+      height: 99% !important;
+    }
+  }
+
+  .card {
+    background-color: #f3f3f3;
+    padding: 50px;
+    text-align: center;
+    z-index: 2;
+
+    .card-icons {
+      margin-top: 30px;
+      svg,
+      img {
+        margin: 10px;
+      }
+    }
+  }
+}
+
 </style>
